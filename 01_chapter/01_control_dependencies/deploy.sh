d71bdd55ce6f25200ac7c2fc2ccaa42b0f58336e
Iwan Silvan Bolzern <iwan.bolzern@zuehlke.com>
2021-04-21 13:59:41 +0200

    moved deploy into script

---
+++ 01_chapter/01_control_dependencies/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd zp3_v1
+VER=$(poetry version -s)
+poetry version "${VER}.${BUILD_NR}"
+poetry publish --build -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
+cd ..
+cd zp3_v2
+VER=$(poetry version -s)
+poetry version "${VER}.${BUILD_NR}"
+poetry publish --build -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
+cd ..
