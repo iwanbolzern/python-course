3e1e602015bc4edd8a29b95edd3fd759066cfdbf
Iwan Silvan Bolzern <iwan.bolzern@zuehlke.com>
2021-04-21 14:53:45 +0200

    implemented zp1 & 2

--- 01_chapter/01_control_dependencies/deploy.sh
+++ 01_chapter/01_control_dependencies/deploy.sh
@@ -9,3 +9,13 @@ VER=$(poetry version -s)
 poetry version "${VER}.${BUILD_NR}"
 poetry publish --build -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
 cd ..
+cd zp1
+VER=$(poetry version -s)
+poetry version "${VER}.${BUILD_NR}"
+poetry publish --build -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
+cd ..
+cd zp2
+VER=$(poetry version -s)
+poetry version "${VER}.${BUILD_NR}"
+poetry publish --build -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD}
+cd ..
