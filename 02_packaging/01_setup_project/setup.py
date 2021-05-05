1412b7ba112e820d66d3ef3fbdf493346bfa57c8
Iwan Silvan Bolzern <iwan.bolzern@zuehlke.com>
2021-05-05 12:19:39 +0200

    added 01_setup_project exercise

---
+++ 02_packaging/01_setup_project/setup.py
@@ -0,0 +1,4 @@
+# have a look at https://setuptools.readthedocs.io/en/latest/userguide/declarative_config.html#:~:text=0%20documentation-,Configuring%20setup()%20using,cfg%20files&text=If%20compatibility%20with%20legacy%20builds,cfg%20.
+from setuptools import setup
+
+setup()
